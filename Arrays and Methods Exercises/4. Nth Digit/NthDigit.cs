5ed09a6e1ea153a611e404bc6301ac9eb0cacc6b
Peter Todorov <petodorov@gmail.com>
2017-06-12 16:02:13 +0300

    E 4

---
+++ Arrays and Methods Exercises/4. Nth Digit/NthDigit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class NthDigit
+{
+    static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+        int index = int.Parse(Console.ReadLine());
+        string temp = number.ToString();
+      char a =   temp.ElementAt(temp.Length - index);
+        Console.WriteLine(a);
+
+    } 
+}
+
