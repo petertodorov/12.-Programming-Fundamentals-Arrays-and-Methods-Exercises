7e21de5e01dec2692ca197ff77bf1973739e1cde
Peter Todorov <petodorov@gmail.com>
2017-06-12 15:19:57 +0300

    E 2

---
+++ Arrays and Methods Exercises/2. Min Method/MinMethod.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class MinMethod
+{
+    static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        int second = int.Parse(Console.ReadLine());
+        int third = int.Parse(Console.ReadLine());
+        int b = Math.Min(second, third);
+        int smallest = GetMin(a, b);
+        Console.WriteLine(smallest);
+    }
+
+    private static int GetMin(int a, int b)
+    {
+        int result = Math.Min(a, b);
+        return result;
+    }
+}
+
