3978f907bb2646440d922baabd307c421fb7a2ce
Peter Todorov <petodorov@gmail.com>
2017-06-12 15:05:53 +0300

    E 1

---
+++ Arrays and Methods Exercises/1. Hello, Name!/HelloName.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class HelloName
+{
+    static void Main(string[] args)
+    {
+        string name= Console.ReadLine();
+        PrintName(name);
+    }
+
+    private static void PrintName(string name)
+    {
+        Console.WriteLine($"Hello {name}!");
+    }
+}
+
