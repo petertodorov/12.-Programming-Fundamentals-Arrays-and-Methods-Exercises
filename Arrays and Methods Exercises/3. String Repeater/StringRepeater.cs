5a55b997db527c47fac6ffc980e38a8b2126df97
Peter Todorov <petodorov@gmail.com>
2017-06-12 15:24:41 +0300

    E 3

---
+++ Arrays and Methods Exercises/3. String Repeater/StringRepeater.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class StringRepeater
+{
+    static void Main(string[] args)
+    {
+        string str = Console.ReadLine();
+        int count = int.Parse(Console.ReadLine());
+        string result = RepeatString(str, count);
+        Console.WriteLine(result);
+    }
+
+    private static string RepeatString(string str, int count)
+    {
+        string result = "";
+        for (int i = 0; i < count; i++)
+        {
+            result += str;
+        }
+        return result;
+    }
+}
+
