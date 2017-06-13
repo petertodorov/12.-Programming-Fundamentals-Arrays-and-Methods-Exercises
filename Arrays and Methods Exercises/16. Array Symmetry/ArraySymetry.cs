6032476ffd80b23c714ba890800e456151fb0cae
Peter Todorov <petodorov@gmail.com>
2017-06-13 15:50:32 +0300

    E 16

---
+++ Arrays and Methods Exercises/16. Array Symmetry/ArraySymetry.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class ArraySymetry
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(' ').ToArray();
+        bool isSymetrical = false;
+
+        for (int i = 0; i < input.Length / 2; i++)
+        {
+            isSymetrical = false;
+            while (input[i] == input[input.Length - 1-i])
+            {
+                isSymetrical = true;
+                break;
+            }
+        }
+        if (isSymetrical)
+        {
+            Console.WriteLine("Yes");
+        }
+        else
+        {
+            Console.WriteLine("No");
+
+        }
+    }
+}
+
