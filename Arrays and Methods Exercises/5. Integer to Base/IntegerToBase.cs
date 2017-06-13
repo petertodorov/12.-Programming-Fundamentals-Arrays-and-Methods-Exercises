7c5f8c1f678126a891b1181524704accff4b3117
Peter Todorov <petodorov@gmail.com>
2017-06-13 10:53:32 +0300

    E 5

---
+++ Arrays and Methods Exercises/5. Integer to Base/IntegerToBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class IntegerToBase
+{
+    static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+        int tobBase = int.Parse(Console.ReadLine());
+        string result = "";
+        while (number!=0)
+        {
+            int remainder = number % tobBase;
+            result = result.Insert(0, Convert.ToString(remainder));
+            number = number / tobBase;
+        }
+        Console.WriteLine(result);
+    }
+}
