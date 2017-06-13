1f2f2d0e9f70b969ef0097317196b83c5e58de3d
Peter Todorov <petodorov@gmail.com>
2017-06-13 13:21:22 +0300

    E 6

--- Arrays and Methods Exercises/5. Integer to Base/IntegerToBase.cs
+++ Arrays and Methods Exercises/5. Integer to Base/IntegerToBase.cs
@@ -9,14 +9,20 @@ class IntegerToBase
     static void Main(string[] args)
     {
         int number = int.Parse(Console.ReadLine());
-        int tobBase = int.Parse(Console.ReadLine());
-        string result = "";
-        while (number!=0)
-        {
-            int remainder = number % tobBase;
-            result = result.Insert(0, Convert.ToString(remainder));
-            number = number / tobBase;
-        }
+        int toBase = int.Parse(Console.ReadLine());
+        string result = IntegerConvertedToBase(number, toBase);
         Console.WriteLine(result);
+    }
+
+    private static string IntegerConvertedToBase(int number, int toBase)
+    {
+        string tempResult = "";
+        while (number != 0)
+        {
+            int remainder = number % toBase;
+            tempResult = tempResult.Insert(0, Convert.ToString(remainder));
+            number = number / toBase;
+        }
+        return tempResult;
     }
 }
---
+++ Arrays and Methods Exercises/6. Notifications/Notifications.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Notifications
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        string result = "";
+        string operation = "";
+        string message = "";
+        string toShow = "";
+        for (int i = 0; i < n; i++)
+        {
+            result = Console.ReadLine();
+            if (result=="success")
+            {
+                operation = Console.ReadLine();
+                message = Console.ReadLine();
+                toShow = ShowSuccess(operation, message);
+                Console.WriteLine(toShow);
+            }
+            else if (result =="error")
+            {
+                operation = Console.ReadLine();
+                int code= int.Parse(Console.ReadLine());
+                toShow = ShowError(operation, code);
+                Console.WriteLine(toShow);
+            }
+            else
+            {
+                continue;
+            }
+        }
+    }
+
+    private static string ShowError(string operation, int code)
+    {
+        string reason = "";
+        if (code>0)
+        {
+            reason = "Invalid Client Data";
+        }
+        else
+        {
+            reason = "Internal System Failure";
+        }
+        string[] temp = new string[] { "Error: Failed to execute ", operation, ".", "\n", "==============================", "\n", "Error Code: ", Convert.ToString(code), ".", "\n","Reason: ", reason, "."};
+        string result = string.Join("", temp);
+        return result;
+    }
+
+    private static string ShowSuccess(string operation, string message)
+    {
+        string[] temp = new string[] {"Successfully executed ", operation, ".","\n", "==============================", "\n","Message: ", message, "." };
+        string result = string.Join("", temp);
+        return result;
+    }
+}
