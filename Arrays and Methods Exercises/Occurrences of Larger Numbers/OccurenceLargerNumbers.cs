76a1c92b44a1ce22ff49e0900deda84c12755b42
Peter Todorov <petodorov@gmail.com>
2017-06-13 14:51:32 +0300

    E 12

---
+++ Arrays and Methods Exercises/Occurrences of Larger Numbers/OccurenceLargerNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class OccurenceLargerNumbers
+{
+    static void Main(string[] args)
+    {
+        double[] input = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+        double check = double.Parse(Console.ReadLine());
+        int counter = 0;
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i]>check)
+            {
+                counter++; 
+            }
+        }
+        Console.WriteLine(counter);
+    }
+}
+
