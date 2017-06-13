4d91591658a4f29233560a323489cc6c0a432a12
Peter Todorov <petodorov@gmail.com>
2017-06-13 15:01:49 +0300

    E 13

---
+++ Arrays and Methods Exercises/13. Increasing Sequence/IncreasingSequenceOfElements.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class IncreasingSequenceOfElements
+{
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        bool not = true;
+        for (int i = 1; i <input.Length; i++)
+        {
+            not = true;
+            if (input[i]<input[i-1])
+            {
+                not=false;
+                break;
+            }
+        }
+        if (!not)
+        {
+            Console.WriteLine("No");
+        }
+        else
+        {
+            Console.WriteLine("Yes");
+        }
+    }
+}
+
