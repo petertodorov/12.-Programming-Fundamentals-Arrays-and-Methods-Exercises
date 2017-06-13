b163b2d36c0189658dc07cbda023d373b7a982cb
Peter Todorov <petodorov@gmail.com>
2017-06-13 14:45:25 +0300

    E 11

--- Arrays and Methods Exercises/10. Count/CountNegativeEllementsInArray.cs
+++ Arrays and Methods Exercises/10. Count/CountNegativeEllementsInArray.cs
@@ -10,12 +10,10 @@ class CountNegativeEllementsInArray
     {
         int n = int.Parse(Console.ReadLine());
         int[] input = new int[n];
-    
         for (int i = 0; i < n; i++)
         {
             input[i] = int.Parse(Console.ReadLine());
         }
-        
         Console.WriteLine(input.Count(x => x < 0));
     }
 }
---
+++ Arrays and Methods Exercises/11. CountGivenElementArray/CountOfGivenElementInArray.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class CountOfGivenElementInArray
+{
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine(input.Count(x => x == n));
+    }
+}
+
