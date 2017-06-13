71b7669a6cd08fbfa006e98bc867de245a8cb194
petertodorov <petodorov@gmail.com>
2017-06-13 22:35:47 +0300

    E 19

---
+++ Arrays and Methods Exercises/19. Debugging Tricky Strings/DebugingTrickyStrings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class TrickyStrings
+{
+    static void Main(string[] args)
+    {
+        string delimiter = Console.ReadLine();
+        int numberOfStrings = int.Parse(Console.ReadLine());
+        string [] currentString = new string[numberOfStrings];
+        for (int i = 0; i < numberOfStrings; i++)
+        {
+            currentString[i] = Console.ReadLine(); ;
+        }
+        Console.WriteLine(String.Join(delimiter, currentString));
+
+    }
+}
