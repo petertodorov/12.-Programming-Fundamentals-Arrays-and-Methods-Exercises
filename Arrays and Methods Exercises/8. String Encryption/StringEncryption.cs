6963bb2085c5c7322a2d3d236effea68d1266e9e
petertodorov <petodorov@gmail.com>
2017-06-14 13:08:45 +0300

    E 8, but can also use exerc 5 for shorter

---
+++ Arrays and Methods Exercises/8. String Encryption/StringEncryption.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class StringEncryption
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        char letter = new char();
+        int letterToInt = new int();
+        int first = new int();
+        int last = new int();
+        string encryptedResult = "";
+        for (int i = 0; i < n; i++)
+        {
+            letter =char.Parse(Console.ReadLine());
+            letterToInt = Convert.ToInt32(letter);
+            string temp = Convert.ToString(letterToInt);
+            char firstTemp = temp.First();
+            char lastTemp = temp.Last();
+            first =(int) Char.GetNumericValue(firstTemp);
+            last = (int)Char.GetNumericValue(lastTemp);
+            int firstPlusLast = (first * 10) + last;
+            string encoded = Convert.ToString(firstPlusLast);
+            char toApendStart = Convert.ToChar(letterToInt+last);
+            char toApendEnd = Convert.ToChar(letterToInt - first);
+            encoded = encoded.Insert(0, Convert.ToString(toApendStart))+toApendEnd;
+            encryptedResult += encoded;
+        }
+        Console.WriteLine(encryptedResult);
+    }
+}
+
